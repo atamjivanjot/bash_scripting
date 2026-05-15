368f4ae91fe693cfcf9b5a0630c84386b7bdc499
Atamjvanjot <atamjivanjot@gmail.com>
2026-05-15 07:35:31 +0530

    This is the learning for Day2

---
+++ day2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#1. variables declaration
+#name="Jivan"
+#role="Cloud Engineer"
+
+#echo "My name is $name"
+#echo "I want to become a $role"
+
+
+#2.User Input
+#echo "Enter your name:"
+#read name 
+#echo "Hello $name"
+
